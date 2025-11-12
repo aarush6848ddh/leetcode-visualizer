680e2abddebd52ddc219efc0c2a6ccda3b13922e
Aarush Singh <106162023+aarush6848ddh@users.noreply.github.com>
2025-11-11 23:02:04 -0500

    add problems and yt link

--- .next/dev/static/chunks/app_problems_[id]_page_tsx_e9335291._.js
+++ .next/dev/static/chunks/app_problems_[id]_page_tsx_e9335291._.js
@@ -1,7 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/_bdae89fa._.js",
+  "static/chunks/components_d827d11b._.js",
+  "static/chunks/_0e3db240._.js",
   "static/chunks/node_modules_de536f1c._.js"
 ],
     source: "dynamic"
